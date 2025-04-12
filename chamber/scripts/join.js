c6f76f5863fee0cce01eee468f47c7a2d7d0afdd
LetMahomed <ldesousa@byupathway.edu>
2025-04-11 22:50:16 -0300

    corrections

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -56,3 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+// Set current year in footer
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+  });
+  
